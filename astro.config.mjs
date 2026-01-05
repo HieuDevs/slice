cfb2ab15260409d2d83e5595cc26facc3661f92c
hieu.bh1 <hieu.bh1@kiotviet.com>
2026-01-05 15:18:02 +0700

    Integrate Vercel adapter into Astro configuration, update default language to Vietnamese, and add .vercel to .gitignore. Update package dependencies in package.json and package-lock.json.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [mdx(), sitemap()],
 
   server: {
@@ -29,7 +32,7 @@ export default defineConfig({
       PUBLIC_DEFAULT_LANG: {
         context: 'client',
         type: 'string',
-        default: 'en',
+        default: 'vi',
         access: 'public',
       },
     },
